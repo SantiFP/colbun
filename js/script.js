a77e0da4f7a17264e88c8c3b854f0e8ae09a7069
unknown <santifp1997@gmail.com>
2024-09-11 02:32:55 -0300

    tabla

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* este codigo es para el funcionamiento del acordion */
+/* const accordionItems = document.querySelectorAll('.acordionSection');
+
+    accordionItems.forEach(item => {
+        const acordion = item.querySelector('.acordionItem');
+
+        acordion.addEventListener('click', () => {
+            item.classList.toggle('abierto');
+        });
+}); */
