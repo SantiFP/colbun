92d5b92e1b291050f5cf5b427943f6458e5bd1ba
unknown <santifp1997@gmail.com>
2024-09-12 23:03:06 -0300

    terminado

--- js/script.js
+++ js/script.js
@@ -10,11 +10,34 @@
 }); */
 
 const buttons = document.querySelectorAll(".filterButton");
+const cells = document.querySelectorAll(".openModal");
+const modalContent = document.querySelector(".modal");
+const modal = document.querySelector(".modal-overlay");
+const closeModalButton = document.querySelector('.modalClose');
+
+const closeModal = () => {
+  modal.style.display = "none";
+}
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+closeModalButton.addEventListener("click", closeModal);
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    modal.style.display = "flex";
+  });
+});
+
 
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     buttons.forEach((btn) => (btn.style.backgroundColor = ""));
 
-    event.target.style.backgroundColor = "#e3dede"; // Color nuevo
+    event.target.style.backgroundColor = "#e3dede";
   });
 });
